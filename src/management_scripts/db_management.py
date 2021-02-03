afb8ca1612fefb2ed2c0a178ed19d7bcc481afc8
Davide de Lerma <davide.delerma@dorbit.space>
2021-02-03 17:12:10 +0000

    added users utility

---
+++ src/management_scripts/db_management.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine
+from src.config import DATABASE_URL
+from sqlalchemy.orm import sessionmaker
+from src.orm.user import User
+import click
+from passlib.context import CryptContext
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+db = SessionLocal()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+@click.command()
+@click.argument("email")
+@click.argument("user_name")
+@click.argument("first_name")
+@click.argument("last_name")
+@click.argument("password")
+def add_initial_superuser(email, user_name, first_name, last_name, password):
+    hashed_pass = pwd_context.hash(password)
+    db_user = User(
+        email=email,
+        user_name=user_name,
+        first_name=first_name,
+        last_name=last_name,
+        password=hashed_pass,
+        is_superuser=True,
+    )
+    db.add(db_user)
+    db.commit()
+    return f"user {user_name} added"
+
+
+if __name__ == "__main__":
+    add_initial_superuser()
